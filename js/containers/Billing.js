50cd127c288434fb6430f36657a04bac6e276ccd
Angelo Ashmore <angeloashmore@me.com>
2015-05-08 11:50:12 -1000

    Removing all logic from Billing page

--- js/containers/Billing.js
+++ js/containers/Billing.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import reactMixin from 'react-mixin';
-
-import Parse from '../stores/Parse';
 
 import AuthenticatedComponent from '../decorators/AuthenticatedComponent';
 
 class Billing extends React.Component {
   render() {
     return(
-      <div>
-        <form>
-          <input type="text" valueLink={this.linkState('plan')} placeholder="Plan ID" />
-          <button type="submit" onClick={this.buttonHandler.bind(this)}>Submit</button>
-        </form>
-      </div>
+      <div>Billing page</div>
     );
   }
 }
-
-reactMixin(Billing.prototype, React.addons.LinkedStateMixin);
 
 export default AuthenticatedComponent(Billing);
