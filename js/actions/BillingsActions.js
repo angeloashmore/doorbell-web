2f5e8edeb979528e0f2228177dffda5161b9a0f6
Angelo Ashmore <angeloashmore@me.com>
2015-07-09 16:55:22 -1000

    Converting billing to doorbell-api

--- js/actions/BillingsActions.js
+++ js/actions/BillingsActions.js
@@ -3,12 +3,44 @@ import Parse from 'lib/Parse';
 
 class BillingsActions {
   fetchAllForCurrentUser() {
-    return Promise.resolve().then(() => {
-      const query = new Parse.Query("Billing");
-      return query.find();
+    const { jwt } = UserStore.getState();
+    if (!jwt) throw new UserNotLoggedIn();
 
-    }).then((results) => {
-      this.dispatch(results);
+    return Promise.resolve().then(() => {
+      return fetch("http://localhost:5000/api/v1/billings", {
+        headers: {
+          "Authorization": `Bearer ${jwt}`
+        }
+      });
+
+    }).then((response) => {
+      return response.json();
+
+    }).then((json) => {
+      this.dispatch(json.billings);
+
+    });
+  }
+
+  update(id, attrs) {
+    return Promise.resolve().then(() => {
+      const { jwt } = UserStore.getState();
+      return fetch(`http://localhost:5000/api/v1/billings/${id}`, {
+        method: "put",
+        headers: {
+          "Authorization": `Bearer ${jwt}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: attrs.email
+        })
+      });
+
+    }).then((response) => {
+      return response.json();
+
+    }).then((billing) => {
+      this.dispatch(billing);
 
     });
   }
